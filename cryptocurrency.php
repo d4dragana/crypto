cd464dfdb887b2bae0f004fa4f80828893d98935
d4 <78612913+d4dragana@users.noreply.github.com>
2021-03-25 20:59:28 +0100

    dodani komentarji

--- cryptocurrency.php
+++ cryptocurrency.php
@@ -41,7 +41,7 @@ $crypto = $stmt->fetch();
         <div class="crypto_rating">Trenutna ocena: <span><?php echo number_format($crypto['rating'],1,',',".");
         //number_format($crypto['rating'],število decimalk,'razdelilnik za cela števila',"oznaka za tisočice")
         ?>
-    </span></div>
+            </span></div>
     </div>
 </section>
 
@@ -51,15 +51,15 @@ $crypto = $stmt->fetch();
             <div class="stars">
                 <form action="rate_insert.php" method="post">
                     <input type="hidden" name="id" value="<?php echo $crypto['id'];?>" />
-                    <input class="star star-5" id="star-5" type="radio" name="star" value="5"/>
+                    <input class="star star-5" id="star-5" type="radio" name="star" value="5" />
                     <label class="star star-5" for="star-5"></label>
-                    <input class="star star-4" id="star-4" type="radio" name="star" value="4"/>
+                    <input class="star star-4" id="star-4" type="radio" name="star" value="4" />
                     <label class="star star-4" for="star-4"></label>
-                    <input class="star star-3" id="star-3" type="radio" name="star" value="3"/>
+                    <input class="star star-3" id="star-3" type="radio" name="star" value="3" />
                     <label class="star star-3" for="star-3"></label>
-                    <input class="star star-2" id="star-2" type="radio" name="star" value="2"/>
+                    <input class="star star-2" id="star-2" type="radio" name="star" value="2" />
                     <label class="star star-2" for="star-2"></label>
-                    <input class="star star-1" id="star-1" type="radio" name="star" value="1"/>
+                    <input class="star star-1" id="star-1" type="radio" name="star" value="1" />
                     <label class="star star-1" for="star-1"></label>
                     <input type="submit" value="Glasuj" class="btn btn-primary" />
                 </form>
@@ -67,6 +67,34 @@ $crypto = $stmt->fetch();
         </div>
     </div>
 </div>
+
+<div class="komentarji" id="komentarji">
+    <div class="obrazec">
+        <form action="comment_insert.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $crypto['id'];?>" />
+            <textarea name="content" rows="5" cols="15">
+            </textarea><br />
+            <input type="submit" value="Komentiraj" class="btn btn-primary" />
+        </form>
+
+    </div>
+    <div class="seznam">
+        <?php
+$query = "SELECT * FROM comments WHERE cryptocurrency_id=?";
+$stmt = $pdo->prepare($query);
+$stmt->execute([$id]);
+
+while ($row =$stmt->fetch()){
+    echo '<div class="komentar">';
+    echo '<div class="oseba">'.getFullName($row['user_id']).' ('.date("j. n. Y H:i",strtotime($row['date_modify'])).')</div>';
+    echo '<div class="vsebina">'.$row['content'].'</div>';
+    echo '</div>';
+}
+    ?>
+
+    </div>
+</div>
+
 <?php
 include_once "footer.php";
 ?>
--- session.php
+++ session.php
@@ -9,7 +9,17 @@ if (!isset($_SESSION['user_id'])   && (!in_array($_SERVER['REQUEST_URI'], $allow
 
     header("Location: login.php");
     die();
-
 }
 
+function getFullName($user_id){
+    require "database.php";
+
+    $query= "SELECT * FROM users WHERE id=?";
+    $stmt=$pdo->prepare($query);
+    $stmt->execute([$user_id]);
+
+    $user=$stmt->fetch();
+
+    return $user['first_name'].' '.$user['last_name'];
+}
 ?>
